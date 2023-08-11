d034dfcaab1de1b687d23952162fe3cf0eeec2af
Alex <sorath2@yandex.ru>
2023-08-11 14:20:35 +0500

    ФЗ Спринт13 v.2.0

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -3,7 +3,6 @@ from django.db import transaction
 from djoser.serializers import UserSerializer
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
-from rest_framework.generics import get_object_or_404
 from rest_framework.exceptions import ValidationError
 from rest_framework.validators import UniqueValidator
 
