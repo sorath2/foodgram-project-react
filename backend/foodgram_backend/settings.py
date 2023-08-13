000853419036af045ab61badb58c25408feb077d
Alex <sorath2@yandex.ru>
2023-08-13 17:40:35 +0500

    ФЗ Спринт13 v.2.3

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["foodgramx.ddns.net", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["foodgramx.ddns.net"]
 
 
 INSTALLED_APPS = [
