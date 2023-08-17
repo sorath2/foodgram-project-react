7fccf44d9130072a5affcd0a9e994c6582d5ce9e
Alex <sorath2@yandex.ru>
2023-08-17 21:22:04 +0500

    ФЗ Спринт13 v.2.5

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -66,8 +66,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB", "django"),
-        "USER": os.getenv("POSTGRES_USER", "django_user"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "mysecretpassword"),
+        "USER": os.getenv("POSTGRES_USER", "django"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", ""),
         "HOST": os.getenv("DB_HOST", "db"),
         "PORT": os.getenv("DB_PORT", 5432),
     }
