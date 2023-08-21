20557602bef986b09f07034a4c7fac52e47dcc24
Alex <sorath2@yandex.ru>
2023-08-21 21:55:23 +0500

    ФЗ Спринт13 v.2.5

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,7 +5,7 @@ from djoser.views import UserViewSet as DjoserUserViewSet
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.generics import get_object_or_404
-from rest_framework.permissions import SAFE_METHODS, IsAuthenticated, AllowAny
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
 
 
